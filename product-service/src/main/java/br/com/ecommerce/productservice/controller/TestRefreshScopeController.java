59837f5523a75dc2fa0fd1125f1842be55137c33
Evaristo S Rodrigues <evaristosrodrigues@gmail.com>
2021-03-23 01:26:36 -0300

    Finalizando video 1

---
+++ product-service/src/main/java/br/com/ecommerce/productservice/controller/TestRefreshScopeController.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package br.com.ecommerce.productservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author evaristosrodrigues
+ *
+ */
+@RestController
+@RequestMapping("/api/test")
+@RefreshScope
+public class TestRefreshScopeController {
+	
+	@Value("${test.name}")
+	private String name;
+	
+	@GetMapping
+	public String test() {
+		return name;
+	}
+
+}
